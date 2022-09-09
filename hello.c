391dca562f598911c7ec604e65fbbc5065a642d1
Matthias Hertel <matthias.hertel@arm.com>
2022-09-09 10:58:54 +0200

    Initial commit

---
+++ hello.c
@@ -0,0 +1,20 @@
+/* sorts.c simple sorting program */
+
+/* Copyright (c) 2007-2017 Arm Limited (or its affiliates). All rights reserved. */
+/* Use, modification and redistribution of this file is subject to your possession of a     */
+/* valid End User License Agreement for the Arm Product of which these examples are part of */
+/* and your compliance with all applicable terms and conditions of such licence agreement.  */
+
+
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void)
+{
+
+    printf("Hello World");
+
+    return 0;
+}
